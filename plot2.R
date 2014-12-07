09ddb58a289e38c957b36d12c0bea137b6b4906a
G Man <gbyomuju@opensips.(none)>
2014-12-07 18:10:43 +0000

    Exploratory Data Anlysis

--- plot2.R
+++ plot2.R
@@ -2,14 +2,17 @@
 
 dataFile <- "./household_power_consumption.txt"
 
-# Read the data into a data frame
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 
 # Get only data within the specified dates by using the %in% operator.
 dateRange <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+dateTime <- strptime(sprintf("%s %s",dateRange$Date,dateRange$Time), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(dateRange$Global_active_power)
 
-# Open dev png and provide the output filename
-png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+# open png device and provide output filename.
+png("plot2.png", width=480, height=480)
+
+# Plot by using type lines
+plot(dateTime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
